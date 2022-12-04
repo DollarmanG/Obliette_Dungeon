58d3f416ff598da92989ea1ef4e7e03665389aa9
DollarmanG <noman.syed@yh.nackademin.se>
2022-12-04 10:51:28 +0100

    added a patrolpath script, gizmo and added comments to the script

---
+++ Assets/_Slask Folder/Noman/Scripts/PatrolPath.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace Gizmo
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        //Radius of the gizmo point
+        private const float waypointGizmoRadius = 0.3f;
+
+        //this method increases the waypoint for the path and draws new lines from the previus waypoint to a new point
+        void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                int j = GetNextIndex(i);
+                Gizmos.DrawSphere(GetWaypoint(i), waypointGizmoRadius);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
+            }
+        }
+
+        //this one calculates if there is a waypoint and then adds 1 more waypoint if one is added from the previous one.
+        public int GetNextIndex(int i)
+        {
+            if (i + 1 == transform.childCount)
+            {
+                return 0;
+            }
+            return i + 1;
+        }
+
+        //gets the transform position of the waypoint
+        public Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).position;
+        }
+    }
+}
