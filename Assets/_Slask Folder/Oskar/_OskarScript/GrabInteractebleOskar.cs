b2b5eafb1748b8a508ead89f1edd824bed8549aa
Oskarmollberg2 <71009358+Oskarmollberg2@users.noreply.github.com>
2022-12-05 17:26:58 +0100

    Fixade handlers storlek
    i script fixade jag att cuben inte ändras beroende på hur handlerns storlek är. jag satte även in en jjärn prefab i oskar slask scen.

--- Assets/_Slask Folder/Oskar/_OskarScript/GrabInteractebleOskar.cs
+++ Assets/_Slask Folder/Oskar/_OskarScript/GrabInteractebleOskar.cs
@@ -6,10 +6,12 @@ public class GrabInteractebleOskar : MonoBehaviour
 {
     [SerializeField] private Transform teleportTo;
     [SerializeField] private Transform startPosistion;
+    public Rigidbody rb;
+    [SerializeField] private Rigidbody HandlerRB;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,5 +23,10 @@ public class GrabInteractebleOskar : MonoBehaviour
     {
         startPosistion.position = teleportTo.position;
         startPosistion.rotation = teleportTo.rotation;
+        //startPosistion.localScale = teleportTo.localScale;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        HandlerRB.velocity = Vector3.zero;
+        HandlerRB.angularVelocity = Vector3.zero;
     }
 }
