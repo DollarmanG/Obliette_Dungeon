495c7c515a596f84f617547aedc0e6688513e138
henrymik <113172684+hemi2201@users.noreply.github.com>
2022-12-07 17:40:46 +0100

    Add scaling particle system
    Added intial particle system for the first state that adapts to the size of the rockfall prefab at Start()

--- Assets/_Slask Folder/Henry/HenryScripts/DebrisParticleEffectState0.cs
+++ Assets/_Slask Folder/Henry/HenryScripts/DebrisParticleEffectState0.cs
@@ -9,31 +9,40 @@ public class DebrisParticleEffectState0 : MonoBehaviour
     [SerializeField]
     private Transform rockfallTransform;
 
-    private ParticleSystem particleSystem;
+    private ParticleSystem particleSystemStateZero;
 
     Vector3 rockfallScale;
+
+    float scaleTargetX;
+    float scaleTargetY;
+    float scaleTargetZ;
 
     // Start is called before the first frame update
     void Start()
     {
-        particleSystem = GetComponent<ParticleSystem>();
+        particleSystemStateZero = GetComponent<ParticleSystem>();
 
-        
+        var sh = particleSystemStateZero.shape;
 
-        rockfallScale.x = rockfallTransform.localScale.x;
-        rockfallScale.y = rockfallTransform.localScale.y;
-        rockfallScale.z = rockfallTransform.localScale.z;
+        Debug.Log($"rockfallTransform.localScale.x is {rockfallTransform.localScale.x}");
+        Debug.Log($"rockfallTransform.localScale.y is {rockfallTransform.localScale.y}");
+        Debug.Log($"rockfallTransform.localScale.z is {rockfallTransform.localScale.z}");
 
-        particleSystem.shape.scale.Equals(rockfallTransform.localScale);
+        scaleTargetX = rockfallTransform.localScale.x;
+        scaleTargetY = rockfallTransform.localScale.y;
+        scaleTargetZ = rockfallTransform.localScale.z;
+
+        sh.scale = new Vector3(scaleTargetX, scaleTargetY, scaleTargetZ);
     }
 
     // Update is called once per frame
     void Update()
     {
-        particleSystem.shape.scale.Set(rockfallScale.x, rockfallScale.y, rockfallScale.z);
+        //particleSystem.shape.scale.Set(rockfallScale.x, rockfallScale.y, rockfallScale.z);
 
-        Debug.Log(rockfallTransform.localScale.x);
-        Debug.Log(rockfallTransform.localScale.y);
-        Debug.Log(rockfallTransform.localScale.z);
+        Debug.Log($"Target x value is {scaleTargetX}");
+        Debug.Log($"Target y value is {scaleTargetY}");
+        Debug.Log($"Target z value is {scaleTargetZ}");
+
     }
 }
