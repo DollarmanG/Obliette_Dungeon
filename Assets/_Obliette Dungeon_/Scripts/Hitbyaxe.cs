6710c3ac18183e5b065254ee0aab270b109f40ca
PexiAlexi <alexi.strandberg@yh.nackademin.se>
2022-12-08 18:10:17 +0100

    First part of axe trap added
    Whats left:
    
    Trigger boxes that does stuff.

---
+++ Assets/_Obliette Dungeon_/Scripts/Hitbyaxe.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitbyaxe : MonoBehaviour
+{
+
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       
+        
+            Debug.Log("Du blev yxad");
+      
+
+        
+    }
+
+
+}
