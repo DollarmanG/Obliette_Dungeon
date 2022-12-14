4f0b36437c0445b5c76045f95b13857d3aad8af5
Oskarmollberg2 <71009358+Oskarmollberg2@users.noreply.github.com>
2022-12-14 12:16:02 +0100

    Fade in och komentering av kod
    Fixade så att fade in fungerar ifrån start och att den tiden går att bestäma. har även kommenterat koden så att den går att läsa av alla.

--- Assets/_Slask Folder/Oskar/_OskarScript/FadeScript.cs
+++ Assets/_Slask Folder/Oskar/_OskarScript/FadeScript.cs
@@ -3,22 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FadeScript : MonoBehaviour
-{
+{   
+    //canvas group is neded to get acces to the alpha veriable
     [SerializeField] private CanvasGroup canvasGroup;
+
+    // These are set to false so that the code dose not run whitout input or When its not calld.
     private bool _fadeIn = false;
     private bool _fadeOut = false;
 
-    public float timeToFade;
+    // These are the time veriable that mekes it posible for adjustment in the fade durastion.
+    public float timeToFadeOut;
+    public float timeToFadeIn;
 
-   
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
+        // This is calld first so a fade in happens when the scean is loaded. 
+        FadeIn();
+    }
+    void Update()
+    {   
         if (_fadeIn)
         {
-            if (canvasGroup.alpha < 1)
+            // This cheks if the alpha of the canvas is fully visible and if it is, then it will fade it out under a durastion. When it is att a desigerd fade then it stops fading it
+            if (canvasGroup.alpha <= 1)
             {
-                canvasGroup.alpha += timeToFade * Time.deltaTime;
+                canvasGroup.alpha -= timeToFadeIn * Time.deltaTime;
                 if (canvasGroup.alpha >= 1)
                 {
                     _fadeIn = false;
@@ -27,9 +36,10 @@ public class FadeScript : MonoBehaviour
         }
         if (_fadeOut)
         {
+            // This cheks if the alpha of the canvas is fully invicible and if it is, then it will fade it in under a durastion. When it is att a desigerd fade then it stops defading it
             if (canvasGroup.alpha >= 0)
             {
-                canvasGroup.alpha += timeToFade * Time.deltaTime;
+                canvasGroup.alpha += timeToFadeOut * Time.deltaTime;
                 if (canvasGroup.alpha == 0)
                 {
                     _fadeIn = false;
@@ -37,10 +47,12 @@ public class FadeScript : MonoBehaviour
             }
         }
     }
+    // When this is calld then fade in on update vill will start
     public void FadeIn()
     {
         _fadeIn = true;
     }
+    // When this is calld then fade out on update vill will start
     public void FadeOut()
     {
         _fadeOut = true;
