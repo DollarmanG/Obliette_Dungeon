de5a33af9dae423fe36948bb33d967c8bb69f3e3
DollarmanG <noman.syed@yh.nackademin.se>
2022-12-17 20:27:43 +0100

    updated all my scripts with comments

--- Assets/_Obliette Dungeon_/GameScripts/Core/AI/IInteractable.cs
+++ Assets/_Obliette Dungeon_/GameScripts/Core/AI/IInteractable.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//this interface makes a character to interact with another object.
 public interface IInteractable
 {
     void Interact(GameObject interactor);
